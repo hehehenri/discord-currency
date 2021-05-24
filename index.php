d757b8e388a732b59d1b3f3ae9c765d52d338c3c
henri <henriborgessilva@gmail.com>
2021-05-24 14:37:54 -0300

    eu adicionei tanta coisa que nem sei por onde começar

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ use Discord\Discord;
 
 //Configurando o Discord;
 $discord = new Discord([
-    'token' => 'token',
+    'token' => 'ODQ1NzgwNDM1OTg0NDQ5NTk3.YKl8aQ.CwRpOF8np16_E7s3FiPLBnt6LL0',
 ]);
 
 //-Toda a config da api daqui
-$api_url = 'apiurl';
+$api_url = 'http://data.fixer.io/api/latest?access_key=9685b2f46fb15830a6db760e4ec51651&format=1';
 
 $json = file_get_contents($api_url);
 $data = json_decode($json);
@@ -23,6 +23,39 @@ function usdBrl(){
     global $currency, $exchange;
     $currency = ($exchange->BRL)/($exchange->USD);
     $currency = round($currency, 2);
+    echo PHP_EOL.PHP_EOL."-Requisição de dolar.".PHP_EOL.PHP_EOL;
+    return $currency;
+}
+
+function eurBrl(){
+    global $currency, $exchange;
+    $currency = ($exchange->BRL)/($exchange->EUR);
+    $currency = round($currency, 2);
+    echo PHP_EOL.PHP_EOL."-Requisição de euro.".PHP_EOL.PHP_EOL;
+    return $currency;
+}
+
+function gbpBrl(){
+    global $currency, $exchange;
+    $currency = ($exchange->BRL)/($exchange->GBP);
+    $currency = round($currency, 2);
+    echo PHP_EOL.PHP_EOL."-Requisição de libra.".PHP_EOL.PHP_EOL;
+    return $currency;
+}
+
+function btcBrl(){
+    global $currency, $exchange;
+    $currency = ($exchange->BRL)/($exchange->BTC);
+    $currency = round($currency, 2);
+    echo PHP_EOL.PHP_EOL."-Requisição de BTC.".PHP_EOL.PHP_EOL;
+    return $currency;
+}
+
+function ethBrl(){
+    global $currency, $exchange;
+    $currency = ($exchange->BRL)/($exchange->ETH);
+    $currency = round($currency, 2);
+    echo PHP_EOL.PHP_EOL."-Requisição de ETH.".PHP_EOL.PHP_EOL;
     return $currency;
 }
 
@@ -31,15 +64,36 @@ $discord->on('ready', function ($discord){
 
     $discord->on('message', function ($message, $discord){
         if ($message->content == "!moedas"){
-            $message->channel->sendMessage("  __**Moedas Suportadas:**__".PHP_EOL."-Dolar:".PHP_EOL."```-dolar```");
+            echo PHP_EOL.PHP_EOL."-Consulta.".PHP_EOL.PHP_EOL;
+            $message->channel->sendMessage("  **Moedas Suportadas:**"
+                .PHP_EOL."-Dolar: "."`!dolar`"
+                .PHP_EOL."-Euro: "."`!euro`"
+                .PHP_EOL."-Libra: "."`!libra`"
+                .PHP_EOL."-Bitcoin: ". "`!btc`");
         }
-    });
 
-    $discord->on('message', function ($message, $discord){
         //dolar
         if($message->content === "!dolar"){
-            echo PHP_EOL.PHP_EOL."-Requisição de dolar.".PHP_EOL.PHP_EOL;
             $message->channel->sendMessage("**1** Dolar americano igual a".PHP_EOL."**".usdBrl()."** Real Brasileiro");
+        }
+
+        //euro
+        if($message->content === "!euro"){
+            $message->channel->sendMessage("**1** Euro igual a".PHP_EOL."**".eurBrl()."** Real Brasileiro");
+        }
+
+        //libra
+        if($message->content === "!libra"){
+            $message->channel->sendMessage("**1** Libra esterlina igual a".PHP_EOL."**".gbpBrl()."** Real Brasileiro");
+        }
+
+        //bitcoin
+        if($message->content === "!btc"){
+            $message->channel->sendMessage("**1** Bitcoin igual a".PHP_EOL."**".btcBrl()."** Real Brasileiro");
+        }
+
+        if($message->content === "!eth"){
+            $message->channel->sendMessage("**1** Ether igual a".PHP_EOL."**".ethBrl()."** Real Brasileiro");
         }
     });
 });
